50eb59e753938677b6057c708ae06c08f9be7e91
Adam Wonak <adam.wonak@gmail.com>
2023-07-08 12:35:22 -0500

    cleanup. reduce initialized variables.

--- apps/looper/looper.cpp
+++ apps/looper/looper.cpp
@@ -1,9 +1,9 @@
+#include "looper.h"
+
 #include <algorithm>
 
 #include "daisysp.h"
 #include "kxmx_bluemchen.h"
-
-#include "looper.h"
 
 using namespace kxmx;
 using namespace daisy;
@@ -11,11 +11,8 @@ using namespace daisysp;
 
 Bluemchen bluemchen;
 
-Parameter knob1;
-Parameter knob2;
-Parameter cv1;
-Parameter cv2;
-
+Parameter loop_start;   // Knob 1
+Parameter loop_length;  // Knob 2
 
 // Allocate buffer in SDRAM
 static const uint32_t kBufferLengthSec = 5;
@@ -25,6 +22,8 @@ static float DSY_SDRAM_BSS buffer[kBufferLenghtSamples];
 
 static synthux::Looper looper;
 static PitchShifter pitch_shifter;
+
+int8_t pitch_val;
 
 void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size) {
     for (size_t i = 0; i < size; i++) {
@@ -37,11 +36,8 @@ int main(void) {
     bluemchen.Init();
     bluemchen.StartAdc();
 
-    knob1.Init(bluemchen.controls[bluemchen.CTRL_1], 0.0f, 1.0f, Parameter::LINEAR);
-    knob2.Init(bluemchen.controls[bluemchen.CTRL_2], 0.0f, 1.0f, Parameter::LINEAR);
-
-    cv1.Init(bluemchen.controls[bluemchen.CTRL_3], 0.0f, 1.0f, Parameter::LINEAR);
-    cv2.Init(bluemchen.controls[bluemchen.CTRL_4], 0.0f, 1.0f, Parameter::LINEAR);
+    loop_start.Init(bluemchen.controls[bluemchen.CTRL_1], 0.0f, 1.0f, Parameter::LINEAR);
+    loop_length.Init(bluemchen.controls[bluemchen.CTRL_2], 0.0f, 1.0f, Parameter::LINEAR);
 
     bluemchen.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
     float samplerate = bluemchen.AudioSampleRate();
@@ -56,18 +52,16 @@ int main(void) {
 
     for (;;) {
         bluemchen.ProcessAllControls();
-        
+
         // Set loop parameters
-        float loop_start = knob1.Value();
-        float loop_length = knob2.Value();
-        looper.SetLoop(loop_start, loop_length);
+        looper.SetLoop(loop_start.Value(), loop_length.Value());
 
         // Toggle record
         looper.SetRecording(bluemchen.encoder.RisingEdge());
 
         // Set pitch - clamp value within a ±12 semitone range.
-        int read = bluemchen.encoder.Increment();
-        float pitch_val = std::clamp(read, -12, 12);
-        pitch_shifter.SetTransposition(pitch_val / 12);
+        int8_t read = bluemchen.encoder.Increment();
+        pitch_val = std::clamp(pitch_val + read, -12, 12);
+        pitch_shifter.SetTransposition((float)pitch_val / (float)12);
     }
 }
